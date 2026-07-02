e8f9e678d44c7eaedc02d725c9a91a7f10f66002
Shiva Kumar <shivakumar.jalla56@gmail.com>
2026-07-02 19:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// Create the student object
+const student = {
+  name: "John",
+  age: 24,
+  city: "Hyderabad",
+};
+
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Testing the function
+console.log(getKeys(student)); // Output: ["name"]
